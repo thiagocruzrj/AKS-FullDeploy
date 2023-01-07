a8394165f0ca61282d2f1e6c50dafeb7922f3ad9
Thiago Cruz <thagocruz@gmail.com>
2023-01-06 21:06:05 -0300

    add redis

---
+++ terraform/redis.tf
@@ -0,0 +1,16 @@
+resource "azurerm_redis_cache" "aksredis" {
+  name                = "${var.dns_prefix}-${random_integer.random_int.result}-redis"
+  location            = azurerm_resource_group.aksrg.location
+  resource_group_name = azurerm_resource_group.aksrg.name
+  capacity            = 0
+  family              = "C"
+  sku_name            = "Basic"
+  enable_non_ssl_port = true
+  redis_configuration {
+  }
+  
+  tags = {
+    environment = var.environment
+    project     = "azbgaks"
+  }
+}
