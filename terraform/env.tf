836d42acd1eb7c971f8673dfb2378b7154ba7b3a
Thiago Cruz <thagocruz@gmail.com>
2023-01-06 21:12:00 -0300

    add last required data

--- terraform/env.tf
+++ terraform/env.tf
@@ -168,3 +168,185 @@ resource "azurerm_log_analytics_solution" "akslogs" {
     product   = "OMSGallery/ContainerInsights"
   }
 }
+
+resource "azurerm_kubernetes_cluster" "akstf" {
+  name                 = "${var.dns_prefix}-${random_integer.random_int.result}"
+  location             = azurerm_resource_group.aksrg.location
+  resource_group_name  = azurerm_resource_group.aksrg.name
+  dns_prefix           = var.dns_prefix
+  kubernetes_version   = var.kubernetes_version
+  private_link_enabled = true
+  node_resource_group  = "${azurerm_resource_group.aksrg.name}_nodes_${azurerm_resource_group.aksrg.location}"
+  linux_profile {
+    admin_username = "phoenix"
+
+    ssh_key {
+      key_data = file("${var.ssh_public_key}")
+    }
+  }
+
+  default_node_pool {
+    name                = "default"
+    node_count          = 1
+    vm_size             = "Standard_DS2_v2" #"Standard_F4s" # Standard_DS2_v2
+    os_disk_size_gb     = 120
+    max_pods            = 30
+    vnet_subnet_id      = azurerm_subnet.aksnet.id
+    type                = "VirtualMachineScaleSets"
+    enable_auto_scaling = true
+    min_count           = 1
+    max_count           = 4
+  }
+
+  network_profile {
+    network_plugin     = "azure"
+    service_cidr       = "10.2.0.0/24"
+    dns_service_ip     = "10.2.0.10"
+    docker_bridge_cidr = "172.17.0.1/16"
+    load_balancer_sku  = "standard"
+  }
+
+  identity {
+    type = SystemAssigned
+  }
+
+  addon_profile {
+    oms_agent {
+      enabled                    = true
+      log_analytics_workspace_id = azurerm_log_analytics_workspace.akslogs.id
+    }
+
+    kube_dashboard {
+      enabled = true
+    }
+  }
+
+  tags = {
+    environment = var.environment
+    project     = "azbgaks"
+  }
+}
+
+# Create Static Public IP Address to be used by Nginx Ingress
+resource "azurerm_public_ip" "nginx_ingress-stage" {
+  name                = "nginx-ingress-pip-stage"
+  location            = azurerm_kubernetes_cluster.akstf.location
+  resource_group_name = azurerm_kubernetes_cluster.akstf.node_resource_group
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  domain_name_label   = "${var.dns_prefix}-${random_integer.random_int.result}-stage"
+
+  depends_on = [azurerm_kubernetes_cluster.akstf]
+}
+
+resource "kubernetes_namespace" "nginx-ns" {
+  metadata { name = "nginx" }
+
+  depends_on = [azurerm_kubernetes_cluster.akstf]
+}
+
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.akstf.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.akstf.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.akstf.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.akstf.kube_config.0.cluster_ca_certificate)
+}
+
+# https://www.terraform.io/docs/providers/helm/index.html
+provider "helm" {
+  kubernetes {
+    load_config_file       = false
+    host                   = azurerm_kubernetes_cluster.akstf.kube_config.0.host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.akstf.kube_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.akstf.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.akstf.kube_config.0.cluster_ca_certificate)
+    config_path            = "ensure-that-we-never-read-kube-config-from-home-dir"
+  }
+}
+
+# https://www.terraform.io/docs/providers/helm/repository.html
+data "helm_repository" "stable" {
+  name = "stable"
+  url  = "https://kubernetes-charts.storage.googleapis.com"
+}
+
+# Install Nginx Ingress using Helm Chart
+# https://www.terraform.io/docs/providers/helm/release.html
+resource "helm_release" "nginx_ingress" {
+  name         = "nginx-ingress"
+  repository   = data.helm_repository.stable.metadata.0.name
+  chart        = "nginx-ingress"
+  namespace    = "nginx"
+  force_update = "true"
+  timeout      = "500"
+
+  set {
+    name  = "controller.service.externalTrafficPolicy"
+    value = "Local"
+  }
+
+  set {
+    name  = "controller.service.loadBalancerIP"
+    value = azurerm_public_ip.nginx_ingress.ip_address
+  }
+
+  set {
+    name  = "controller.replicaCount"
+    value = "2"
+  }
+
+  set {
+    name  = "controller.metrics.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "controller.stats.enabled"
+    value = "true"
+  }
+
+  depends_on = [azurerm_kubernetes_cluster.akstf, kubernetes_namespace.nginx-ns, azurerm_public_ip.nginx_ingress]
+}
+
+resource "null_resource" "get-credentials" {
+  provisioner "local-exec" {
+    command = "az aks get-credentials --resource-group ${azurerm_resource_group.aksrg.name} --name ${azurerm_kubernetes_cluster.akstf.name}"
+  }
+  depends_on = [azurerm_kubernetes_cluster.akstf]
+}
+
+# set env variables for scripts
+resource "null_resource" "set-env-vars" {
+  provisioner "local-exec" {
+    command = "export KUBE_GROUP=${azurerm_resource_group.aksrg.name}; export KUBE_NAME=${azurerm_kubernetes_cluster.akstf.name}; export LOCATION=${var.location}"
+  }
+  depends_on = [azurerm_kubernetes_cluster.akstf]
+}
+
+output "NODE_GROUP" {
+  value = "${azurerm_resource_group.aksrg.name}_nodes_${azurerm_resource_group.aksrg.location}"
+}
+
+output "ID" {
+  value = azurerm_kubernetes_cluster.akstf.id
+}
+
+output "PUBLIC_IP" {
+  value = azurerm_public_ip.nginx_ingress.ip_address
+}
+
+output "PUBLIC_IP_STAGE" {
+  value = azurerm_public_ip.nginx_ingress-stage.ip_address
+}
+
+output "instrumentation_key" {
+  value = azurerm_application_insights.aksainsights.instrumentation_key
+}
+
+output "AZURE_CONTAINER_REGISTRY_NAME" {
+  value = azurerm_container_registry.aksacr.name
+}
+
+output "AZURE_KEYVAULT_NAME" {
+  value = azurerm_key_vault.aksvault.name
+}
